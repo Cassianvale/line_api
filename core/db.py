b7ddd938c45d49565c2514c5492a474191d80787
Cassianvale <1544257291@qq.com>
2024-04-25 17:43:11 +0800

    init

--- core/db.py
+++ core/db.py
@@ -1,22 +1,70 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import redis
+from sqlmodel import SQLModel, create_engine, Session
+from sqlalchemy.exc import SQLAlchemyError
 from config.setting import settings
 from datetime import datetime
-from sqlalchemy import create_engine, Column, Integer, DateTime
-from sqlalchemy.orm import as_declarative, declared_attr
 from utils.log_control import ERROR
 
-# 加载环境变量
-engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
-# 测试数据库连接
-try:
-    engine.connect()
-    print(f"连接数据库成功!")
-except Exception as e:
-    print(f"连接数据库错误，错误信息：{e}")
-    ERROR.logger.error(e)
-    raise
+class MysqlManage:
+
+    @staticmethod
+    def connect_to_database():
+        # Create an engine instance
+        engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI), echo=True, pool_pre_ping=True)
+        return engine
+
+    def test_connect(self) -> None:
+        """
+        Test the connection to the database
+        """
+        try:
+            # Create a new database session
+            engine = self.connect_to_database()
+            with Session(engine) as session:
+                # Attempt to execute a simple query
+                result = session.execute("SELECT 1")
+                if result:
+                    print("MySQL 连接成功")
+                else:
+                    print("MySQL 连接失败")
+        except SQLAlchemyError as e:
+            # Catch and handle any SQLAlchemy errors
+            raise SQLAlchemyError(f"MySQL 连接失败: {e}")
 
 
+class RedisManager:
+    @staticmethod
+    def connect_to_database():
+        """
+        连接 redis 数据库
+        """
+        return redis.Redis(
+            host=settings.REDIS_HOST,
+            port=settings.REDIS_PORT,
+            db=settings.REDIS_DB,
+            password=settings.REDIS_PASSWORD)
+
+    def test_connect(self) -> None:
+        """
+        测试连接
+        :return:
+        """
+        try:
+            # 发送PING命令
+            response = self.connect_to_database().ping()
+            if response:
+                print("Redis 连接成功")
+            else:
+                print("Redis 连接失败")
+        except redis.exceptions.RedisError as e:
+            # 捕获并处理任何Redis错误
+            raise redis.exceptions.RedisError(f"Redis 连接失败: {e}")
+
+
+if __name__ == '__main__':
+    MysqlManage().test_connect()
+    RedisManager().test_connect()
